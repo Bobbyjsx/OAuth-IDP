364febf22e748829434f432222bdb3727332448d
Godswill Ezeala <104563882+Bobbyjsx@users.noreply.github.com>
2026-08-16 15:00:15 +0100

    chore: add framer-motion, tanstack-query, axios, zod, sonner dependencies

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+// https://storage.googleapis.com/bobs-public-store/cognito-logo-icon.svg"
